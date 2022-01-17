2c711bf3f6394c28f6b8a1e0a8ddeafbeecd2457
Wasun M <wasun_08@hotmail.com>
2022-01-17 11:42:19 +0700

    files upload function

--- back-end/index.js
+++ back-end/index.js
@@ -5,9 +5,11 @@ const cors = require("cors");
 const http = require('http');
 const PORT = process.env.PORT || 3001;
 const jwt = require('jsonwebtoken');
-
+const fileUpload = require('express-fileupload');
 const crypto = require('crypto');
 const assert = require('assert');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
 // const users = require('./controller/userController')
 // const register = require('./controller/registerController')
@@ -19,7 +21,11 @@ const functions = require('./functions')
 const dbConfig = require('./config/db')
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload({
+    createParentPath: true
+}));
 
 app.listen(PORT, function () {
     console.log(`server is running ${PORT}`);
@@ -153,6 +159,42 @@ app.get("/jobs", async (req, res) => {
     });
 })
 
+app.post('/api/photos', async (req, res) => {
+    try {
+        if (!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            let response = []
+            let files = req.files.files;
+            console.log("files --> ", files)
 
+            if (files.length > 1) {
+                for (const f of files) {
+                    let name = f.name.split(".")[0]
+                    f.mv('./assets/uploads/' + name + ".jpg");
+                }
+            } else {
+                let name = files.name.split(".")[0]
+                files.mv('./assets/uploads/' + name + ".jpg");
+            }
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: response
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
+app.get('/api/photos', async (req, res) => {
+    const filename = req.query.filename
+    const contents = fs.readFileSync(`./assets/uploads/${filename}.jpg`, { encoding: 'base64' });
+    res.send(contents)
+})
 // app.use('/api/users', [users, register, clients, contractors]);
